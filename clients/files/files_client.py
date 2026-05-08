8187fadfd33313dde5a61960b30b9416b7f75b1c
SergeyAstrx <sergey.tsmyg7.gmail.com>
2026-05-08 21:34:19 +0300

    feat(clients): add FilesClient

---
+++ clients/files/files_client.py
@@ -0,0 +1,48 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.api_client import APIClient
+
+
+class CreateFileRequestDict(TypedDict):
+    filename: str
+    directory: str
+    upload_file: str
+
+
+class FilesClient(APIClient):
+    """
+    Client for endpoint /api/v1/files
+    """
+
+    def get_file_api(self, file_id: str) -> Response:
+        """
+        Method downloads file
+
+        :param file_id: File id
+        :return: Endpoint response (httpx.Response)
+        """
+        return self.get(f"/api/v1/files/{file_id}")
+
+    def create_file_api(self, request: CreateFileRequestDict) -> Response:
+        """
+        Method uploads file
+
+        :param request: Upload file payload
+        :return: Endpoint response (httpx.Response)
+        """
+        return self.post(
+            "/api/v1/files",
+            data=request,
+            files={"upload_file": open(request['upload_file'], 'rb')}
+        )
+
+    def delete_file_api(self, file_id: str) -> Response:
+        """
+        Method deletes file
+
+        :param file_id: File id
+        :return: Endpoint response (httpx.Response)
+        """
+        return self.delete(f"/api/v1/files/{file_id}")
